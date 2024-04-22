2740582c0f3baf2c0dd7023a5eef806e9993a35f
massimoronchini <165090025+massimoronchini@users.noreply.github.com>
2024-04-22 12:23:52 +0200

    Update Rcode_tpmod.R

--- Rcode_tpmod.R
+++ Rcode_tpmod.R
@@ -1,84 +1,86 @@
-# Caricamento delle librerie
+# Carico le librerie necessarie
 library(readxl)
-library(topicmodels)
 library(tm)
 library(dplyr)
-library(spacyr)
-library(slam)
-library(ldatuning)
 library(tidytext)
-library(tidyverse)
-library(stringr)
-library(gutenbergr)
+library(igraph)
+library(ggraph)
+library(topicmodels)
+library(LDAvis)
+library(reshape2)
 library(pheatmap)
+library(spacyr)
 library(writexl)
 
 # Installazione e inizializzazione di spaCy
 spacy_install()
 spacy_initialize(model = "en_core_web_sm")
 
-# Imposta il seed per la riproducibilità all'inizio dello script
-set.seed(123)
-
 # Importo i dati
-df <- read_excel("C:/Users/Utente/OneDrive/DOTTORATO/Progetto di ricerca/Analisi review in R/bib.xlsx", sheet = "clear")
-
-# Creo il dataframe con gli abstract
-df_abstract <- data.frame(AB = df$AB)
-
-# Preparazione dei dati per spacyr
-df_abstract$text <- as.character(df_abstract$AB)
-df_abstract$text <- sapply(df_abstract$text, trimws)
+df <- read_excel("C:/Users/Utente/OneDrive/DOTTORATO/Systematic literature review/Review protocol files/bib.xlsx", sheet = "bib")
+abstracts <- data.frame(AB = df$AB)
 
 # Lemmatizzazione con spacyr
-lemmatized <- spacy_parse(df_abstract$text, lemma = TRUE)
-lemmatized$lemma <- sapply(lemmatized$lemma, trimws)
+seed = 123
+lemmatized <- spacy_parse(abstracts$AB, lemma = TRUE)
 
 # Aggregazione dei lemmi in una singola stringa per documento
+seed = 123
 aggregated_texts <- lemmatized %>%
   group_by(doc_id) %>%
   summarise(text = paste(lemma, collapse=" ")) %>%
   ungroup()
 
 # Crea il corpus dal testo lemmatizzato aggregato
-corpus <- Corpus(VectorSource(aggregated_texts$text))
+seed = 123
+docs <- Corpus(VectorSource(aggregated_texts$text))
 
-# Esegui processi di pulizia dei testi
-corpus <- tm_map(corpus, content_transformer(tolower))
-corpus <- tm_map(corpus, removePunctuation)
-corpus <- tm_map(corpus, removeNumbers)
-termini_specifici <- c()  # Aggiungi qui i termini da escludere
-corpus <- tm_map(corpus, removeWords, c(stopwords("english"), termini_specifici))
-corpus <- tm_map(corpus, stripWhitespace)
-corpus <- tm_map(corpus, content_transformer(function(x) trimws(x)))
+# Aggiunge parole specifiche alla lista delle stopwords ("paper", "research", "study", "case", "literature", "article", "findings")
+seed = 123
+custom_stopwords <- c(stopwords("english"), "paper", "article", "research")
 
-# Creazione della matrice dei termini
-dtm <- DocumentTermMatrix(corpus)
+# Pre-processing
+seed = 123
+docs <- tm_map(docs, content_transformer(tolower))
+docs <- tm_map(docs, content_transformer(removePunctuation))
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, removeWords, custom_stopwords)  # Usa la lista aggiornata qui
+docs <- tm_map(docs, stripWhitespace)
+docs <- tm_map(docs, content_transformer(trimws))
 
-# Esecuzione della ricerca del numero di topic ottimale senza parallelismo
-result <- FindTopicsNumber(
-  dtm,
-  topics = seq(from = 2, to = 30, by = 1),
-  metrics = c("Griffiths2004", "CaoJuan2009", "Arun2010", "Deveaud2014"),
-  method = "Gibbs",
-  control = list(seed = 123),
-  verbose = TRUE)
+# Verifica il testo trasformato per i primi documenti
+inspect(docs[1:5])
 
-# Plot dei risultati per trovare il numero ottimale di topic
-FindTopicsNumber_plot(result)
+# Genero la dtm
+seed = 123
+dtm <- DocumentTermMatrix(docs)
 
-# Creazione del modello LDA con il numero di topic deciso
-# Nota: è importante rieffettuare set.seed() qui se hai eseguito altre operazioni casuali nel frattempo
+# Lda
 set.seed(123)
-lda_model <- LDA(dtm, k = 4, method = "Gibbs", control = list(seed = 123, burnin = 50000, iter = 1000000, thin = 1000))
-terms(lda_model, 30) # Visualizza i primi termini per topic
-topics(lda_model) # Visualizza la distribuzione dei topic nei documenti
+lda_model <- LDA(dtm, k = 5, method = "Gibbs", set.seed(123))
 
-# Ottieni la distribuzione dei topic nei documenti
-topic_distribution <- posterior(lda_model)$topics
-# Esempio di rinominazione basato su un'interpretazione dei topic
-colnames(topic_distribution) <- c("Measurement perspective", "Decision making analysis", "Method and framework", "Community management")
-# Usa pheatmap con le nuove etichette
+# Esplorazione
+seed = 123
+topics <- terms(lda_model, 30) # Ottiene le prime 10 parole per ogni topic
+topics
+
+# Assegnazione dei topic ai documenti (corretto)
+doc_topics <- posterior(lda_model)$topics
+
+# Visualizzazioni
+
+# SerVis(json_lda)
+seed = 123
+json_lda <- createJSON(phi = posterior(lda_model)$terms, 
+                       theta = posterior(lda_model)$topics, 
+                       doc.length = rowSums(as.matrix(dtm)), 
+                       vocab = colnames(as.matrix(dtm)), 
+                       term.frequency = colSums(as.matrix(dtm)))
+serVis(json_lda)
+serVis(json_lda, out.dir = 'C:/Users/Utente/OneDrive/DOTTORATO/Systematic literature review/R analysis/docs', open.browser = TRUE)
+
+# Heatmap con probabilità documento-topic
+topic_distribution <- as.matrix(doc_topics)
 pheatmap(topic_distribution, 
          color = colorRampPalette(c("blue", "yellow"))(100), 
          legend = TRUE, 
@@ -86,3 +88,17 @@ pheatmap(topic_distribution,
          angle_col = 0,
          main = "Topic Distribution across Documents",
          cluster_cols = FALSE)
+
+# Converti topic_distribution in un dataframe
+df_topic_distribution <- as.data.frame(topic_distribution)
+# Aggiungi una colonna con i numeri dei documenti
+# Puoi usare rownames() per ottenere i numeri dei documenti se sono stati definiti come tali
+df_topic_distribution$Document_Number <- rownames(df_topic_distribution)
+# Riordina il dataframe per mettere la colonna dei numeri dei documenti come prima colonna
+df_topic_distribution <- df_topic_distribution[, c(ncol(df_topic_distribution), 1:(ncol(df_topic_distribution)-1))]
+# Converti le probabilità in percentuali, se necessario
+df_topic_distribution[,2:6] <- df_topic_distribution[,2:6] * 100
+# Rinomina le colonne per chiarezza
+colnames(df_topic_distribution) <- c("Document_Number", "Topic 1 (%)", "Topic 2 (%)", "Topic 3 (%)", "Topic 4 (%)", "Topic 5 (%)")
+# Visualizza le prime righe della tabella per verifica
+head(df_topic_distribution)
